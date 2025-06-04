389a21f0687a767aa7eed280711353cdc31d08cc
Dheeraj Kamble <dheeraj.kamble@securelayer7.net>
2025-06-04 15:12:13 +0530

    Add products.json and update products API handler

--- frontend/api/products.js
+++ frontend/api/products.js
@@ -35,6 +35,19 @@ const products = [
       "https://i.dummyjson.com/data/products/2/3.jpg",
       "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
     ]
+  },
+  {
+    "id": 3,
+    "title": "Samsung Universe 9",
+    "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+    "price": 1249,
+    "discountPercentage": 15.46,
+    "rating": 4.09,
+    "stock": 36,
+    "brand": "Samsung",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    "images": ["https://i.dummyjson.com/data/products/3/1.jpg"]
   }
 ];
 
