98cdd37e9ea5757a57295ab6625d1cce12de7eab
Dheeraj Kamble <dheeraj.kamble@securelayer7.net>
2025-06-05 12:05:47 +0530

    Initial commit: React frontend + products API

---
+++ frontend/src/pages/api/products/index.js
@@ -0,0 +1,11 @@
+import { products } from './[id]';
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // If the products array is exported from [id].js, import and return it
+    // Otherwise, copy the products array here
+    res.status(200).json(products);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+} 
